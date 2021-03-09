7758d5b06c39d5e62c597a4afb93fe8307e2814b
coat0 <77875651+coat0@users.noreply.github.com>
2021-03-09 17:42:10 +0100

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+void zbiera(char *tab, int n);
+int main(void){
+    char *lancuch=NULL;
+    int n=0;
+    printf("Podaj ile znakow chcesz pobrac: ");
+    scanf ("%d", &n);
+    while((getchar())!='\n');
+    lancuch=malloc(sizeof(char)*n+1); /*określa rozmiar łańcucha w pamięci*/
+    zbiera(lancuch, n);
+    puts(lancuch);
+    system("pause");
+    free(lancuch); /*opróżnia pamięć*/
+    return 0;
+}
+
+void zbiera(char *tab, int n)
+{
+    int i = 0;
+    {
+        while(i<n)
+        {
+            *tab=getchar();
+            if(!isgraph(*tab)) break;
+            *tab++;
+            i++;
+        }
+        *tab++;
+        *tab='\0';         
+    }
+}  
