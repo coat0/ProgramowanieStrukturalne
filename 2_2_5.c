fe2f0b3c93d1bcffb0651b8f8dcb9cc57bf49448
coat0 <77875651+coat0@users.noreply.github.com>
2021-03-25 17:20:29 +0100

    Add files via upload

--- 2_2_5.c
+++ 2_2_5.c
@@ -3,6 +3,8 @@ int funkcja(int n);
 int main(void){
     int a = 0;
     int wyswietl;
+    printf("Podaj liczbe calkowita,a ja podniose 2 do tej potegi\n");
+    scanf("%d", &a);
     wyswietl = funkcja(a);
     printf("%d\n", wyswietl);
     system("pause");
@@ -10,6 +12,8 @@ int main(void){
 }
 
 funkcja(int n){
-    n = 2;
-    return n;
+    int i;
+    int wynik = 1;
+    for(i=0; i<n; i++) wynik = wynik * 2; 
+    return wynik;
 }
